cf186279a75a739d2f43a6844a7c028396d5d4e2
Alex Muramoto <amuramoto@apache.org>
2017-12-15 13:56:37 -0800

    code cleanup

--- webhook/util.js
+++ webhook/util.js
@@ -1,9 +1,86 @@
 module.exports = {
-  verifyWebhook: verifyWebhook,
-  parseEventType: parseEventType
+  addWebhookVerification: addWebhookVerification,
+  addWebhookReceiver: addWebhookReceiver
 }
 
-function verifyWebhook(qs) {
+function addWebhookVerification (verify_token, app) {
+  app.get('/webhook', (req, res) => {
+    // Parse params from the verification request
+    let verification = verifyWebhook(verify_token, req.query);
+    if (!verification) {
+      res.sendStatus(403);
+    }
+    res.status(200).send(challenge);
+  });
+
+  return;
+}
+
+function addWebhookReceiver (eventEmitter, app, options) {
+  let endpoint = options.endpoint || '/webhook',
+      port = options.port || process.env.PORT,        
+      logging = options.logging;
+
+  // Accepts POST requests at /webhook endpoint
+  app.post('/webhook', (req, res) => {  
+    let body = req.body;
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page' && body.entry) {
+
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+
+      body.entry.forEach(entry => {
+        let webhook_event = entry.messaging[0];        
+        if (logging) console.log('EVENT RECEIVED:\n' + webhook_event);
+        emitWebhookEvent(eventEmitter, webhook_event);
+      });
+      
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  });
+
+  return;
+}
+
+function emitWebhookEvent (eventEmitter, webhook_event) {      
+  let event_type = util.parseEventType(webhook_event);
+  let event_subtype = null;
+  if (event_type === 'messages') {
+    let message = webhook_event.message;
+    if (message.text) {      
+      if (message.quick_reply) {
+        // messages - quick_reply  
+        event_subtype = 'quick_reply';
+      } else {
+        // messages - text    
+        event_subtype = 'text';
+      }
+    } else if (message.attachments) {
+      // messages - attachment      
+      event_subtype = 'attachments';
+   }            
+  } else if (event_type === 'messaging_referrals') {
+    // messaging_referrals - source
+    event_subtype = webhook_event.referral.source;    
+  } else if (event_type === 'messaging_handovers') {    
+    if (webhook_event.pass_thread_control) {
+      // messaging_handovers - pass_thread_control
+      event_subtype = 'pass_thread_control';
+    } else if (webhook_event.take_thread_control) {
+      // messaging_handovers - take_thread_control
+      event_subtype = 'take_thread_control';
+    } else if (webhook_event.app_roles) {
+      // messaging_handovers - app_roles
+      event_subtype = 'app_roles';
+    }   
+  }
+  eventEmitter.emit(event_type, event_subtype, webhook_event);
+}
+
+function verifyWebhook(verify_token, qs) {
   const mode = qs['hub.mode'],
         token = qs['hub.verify_token'],
         challenge = qs['hub.challenge']
@@ -53,5 +130,4 @@ function parseEventType (webhook_event) {
     console.error("Webhook received unknown messagingEvent: ", webhook_event);
     return 'unknown_event';
   }
- 
 }
--- webhook/webhook.js
+++ webhook/webhook.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const express = require('express'),
-      body_parser = require('body-parser'),
-      app = express().use(body_parser.json()),
+      body_parser = require('body-parser'),      
       util = require('./util'),
       EventEmitter = require('events'),
-      emitter = new EventEmitter();
+      emitter = new EventEmitter();      
+
+let app;      
 
 module.exports = {
   createWebhook: createWebhook,
@@ -14,18 +15,14 @@ module.exports = {
 }
 
 function createWebhook (options) {
-  let endpoint = options.endpoint || '/webhook';
-  let port = options.port || process.env.PORT;  
-  let verify_token = options.verify_token;
-  let logging = options.logging;
-  
   // verify_token required
-  if (!verify_token) throw 'VERIFY_TOKEN REQUIRED';
-
-  // Sets server port and logs message on success
-  app.listen(port || process.env.PORT, () => console.log('webhook is listening'));
-  addWebhookReceiver();
-  addWebhookVerification();
+  if (!options.verify_token) throw 'VERIFY_TOKEN REQUIRED';
+  app = express().use(body_parser.json());      
+  util.addWebhookReceiver(emitter, app, options);
+  util.addWebhookVerification(options.verify_token, app);
+  app.listen(options.port || process.env.PORT, () => {
+    console.log('webhook is listening')
+  });
   return app;
 }
 
@@ -33,77 +30,3 @@ function createWebhook (options) {
 function getWebhookInstance () {
   return app;
 }
-
-function addWebhookVerification () {
-  let app = getWebhookInstance();
-  app.get('/webhook', (req, res) => {
-    // Parse params from the verification request
-    let verification = util.verifyWebhook(req.query);
-
-    if (!verification) {
-      res.sendStatus(403);
-    }
-    res.status(200).send(challenge);
-  });
-}
-
-function addWebhookReceiver () {
-  let app = getWebhookInstance();
-  // Accepts POST requests at /webhook endpoint
-  app.post('/webhook', (req, res) => {  
-    let body = req.body;
-    // Check the webhook event is from a Page subscription
-    if (body.object === 'page' && body.entry) {
-
-      // Return a '200 OK' response to all events
-      res.status(200).send('EVENT_RECEIVED');
-
-      body.entry.forEach(entry => {
-        let webhook_event = entry.messaging[0];        
-        if (logging) console.log('EVENT RECEIVED:\n' + webhook_event);
-        emitWebhookEvent(webhook_event);
-      });
-      
-    } else {
-      // Return a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-
-  });
-}
-
-function emitWebhookEvent (webhook_event) {
-  let event_type = util.parseEventType(webhook_event);
-  let event_subtype = null;
-  if (event_type === 'messages') {
-    let message = webhook_event.message;
-    if (message.text) {      
-      if (message.quick_reply) {
-        // messages - quick_reply  
-        event_subtype = 'quick_reply';
-      } else {
-        // messages - text    
-        event_subtype = 'text';
-      }
-    } else if (message.attachments) {
-      // messages - attachment      
-      event_subtype = 'attachments';
-   }            
-  } else if (event_type === 'messaging_referrals') {
-    // messaging_referrals - source
-    event_subtype = webhook_event.referral.source;    
-  } else if (event_type === 'messaging_handovers') {    
-    if (webhook_event.pass_thread_control) {
-      // messaging_handovers - pass_thread_control
-      event_subtype = 'pass_thread_control';
-    } else if (webhook_event.take_thread_control) {
-      // messaging_handovers - take_thread_control
-      event_subtype = 'take_thread_control';
-    } else if (webhook_event.app_roles) {
-      // messaging_handovers - app_roles
-      event_subtype = 'app_roles';
-    }   
-  }
-  emitter.emit(event_type, event_subtype, webhook_event);
-}
-
