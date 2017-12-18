700ac3ef2da141b8921c354baf0057e781ce4fcc
Alex Muramoto <amuramoto@apache.org>
2017-12-18 15:39:33 -0800

    Simplifies the webhook creation process

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 'use strict';
-const webhook = require('./webhook/webhook');
+const webhook = require('./webhook');
 
 module.exports = messenger;
 
 function messenger() {
-  this.Webhook = {
-    create: webhook.createWebhook,
-    events: webhook.emitter,
-    getInstance: webhook.getWebhookInstance
-  }
+  this.Webhook = webhook;
 }
--- webhook/index.js
+++ webhook/index.js
@@ -6,27 +6,24 @@ const express = require('express'),
       EventEmitter = require('events'),
       emitter = new EventEmitter();      
 
-let app;      
+module.exports = Webhook;
 
-module.exports = {
-  createWebhook: createWebhook,
-  emitter: emitter,
-  getWebhookInstance: getWebhookInstance
+function Webhook (options) {  
+  let app = createWebhook(options);
+  this.getEmitter = () => { return emitter };
+  this.getWebhookInstance = () => { return app };  
+  return this;
 }
 
 function createWebhook (options) {
   // verify_token required
   if (!options.verify_token) throw 'VERIFY_TOKEN REQUIRED';
-  app = express().use(body_parser.json());      
+  
+  let app = express().use(body_parser.json());      
   util.addWebhookReceiver(emitter, app, options);
   util.addWebhookVerification(options.verify_token, app);
   app.listen(options.port || process.env.PORT, () => {
     console.log('webhook is listening')
   });
-  return app;
-}
-
-
-function getWebhookInstance () {
   return app;
 }
