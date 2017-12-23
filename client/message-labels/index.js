1aec4d87898e99f2aa233059abc8632c2678930c
Alex Muramoto <amuramoto@apache.org>
2017-12-23 11:57:51 -0800

    Adds constructor for labels

--- client/message-labels/index.js
+++ client/message-labels/index.js
@@ -1,5 +1,44 @@
 function Labels (GraphRequest) {
+  this.create = createLabel;
+  this.get = getLabel;
+  this.getByPsid = getByPsid;
+  this.getAll = getAllLabels;
+  this.delete = deleteLabel;
+  this.removePsid = removePsid;
+  this.addPsid = addPsid;
+  this.send = send.bind(GraphRequest);
+}
 
+function createLabel () {
+
+}
+
+function getLabel () {
+
+}
+
+function getByPsid () {
+
+}
+
+function getAllLabels () {
+
+}
+
+function deleteLabel () {
+
+}
+
+function removePsid () {
+
+}
+
+function addPsid () {
+
+}
+
+function send () {
+  
 }
 
 module.exports = Labels;
