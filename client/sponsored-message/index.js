35788f54a3285209b56c2ae0200c53e130552e7c
Alex Muramoto <amuramoto@apache.org>
2017-12-29 11:08:35 -0800

    Adds support for sponsored message api

---
+++ client/sponsored-message/index.js
@@ -0,0 +1,28 @@
+function SponsoredMessage (GraphRequest) {
+  this.send = send.bind(GraphRequest);
+}
+
+function send (options) {
+  let required = [
+    'message_creative_id',
+    'daily_budget',
+    'bid_amount',
+    'targeting',
+    'ad_account_id'
+  ]
+
+  required.forEach(prop => {
+    if (!options[prop]) {
+      console.error('Valid ' + prop + ' property required');
+      return;
+    }
+  })
+
+  options.access_token = this.getPageToken();
+  options.path = '/act_' + options.ad_account_id + '/sponsored_message_ads';
+  options.api_version = 'v2.11';
+
+  delete options.ad_account_id;
+
+  this.sendGraphRequest({'formData': options});
+}
