06a6744d3c5699bc0f5c55ea6be93213081a9d4f
Alex Muramoto <amuramoto@apache.org>
2017-12-28 17:17:12 -0800

    adds support for since and until to messaging insights

--- client/messaging-insights-api/index.js
+++ client/messaging-insights-api/index.js
@@ -2,17 +2,19 @@ function MessagingInsights (GraphRequest) {
   this.get = get.bind(GraphRequest);
 }
 
-function get(metrics) {
-  if (!metrics) {
+function get(options) {
+  if (!options.metrics) {
     console.error('Valid metrics array required');
     return;
   }
 
   let request_options = {
     'path': '/me/insights',
-    'qs': {'metric': metrics.join(',')}
+    'qs': {'metric': options.metrics.join(',')}
   }
 
+  if (options.since) request_options.qs.since = options.since;
+  if (options.until) request_options.qs.until = options.until;
 
   return this.sendGraphRequest(request_options);
 }
