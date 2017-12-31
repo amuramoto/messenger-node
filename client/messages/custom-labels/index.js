610c7277ecd8e8087f79c52115beb48873c3218b
Alex Muramoto <amuramoto@apache.org>
2017-12-31 13:48:59 -0800

    Adds promise rejections to custom labels

--- client/messages/custom-labels/index.js
+++ client/messages/custom-labels/index.js
@@ -1,106 +1,127 @@
 function createCustomLabel (name) {
-  if (!name) {
-    console.error('name required');
-    return;
-  }
-  let options = {
-    'payload': {'name': name}
-  };
-  return this.callCustomLabelsApi(options);
+  return new Promise (async (resolve, reject) => {
+    if (!name) {
+      reject('name required');
+    }
+    let options = {
+      'payload': {'name': name}
+    };
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function getCustomLabel (label_id, fields) {
-  if (!label_id) {
-    console.error('label_id required');
-    return;
-  }
-  let options = {
-    'path': label_id    
-  }
+  return new Promise (async (resolve, reject) => {
+    if (!label_id) {
+      reject('label_id required');
+    }
+    let options = {
+      'path': label_id    
+    }
 
-  if (fields) {
-    fields = fields.join(',');
-    options.qs = {'fields': fields};
-  }
-  return this.callCustomLabelsApi(options);
+    if (fields) {
+      fields = fields.join(',');
+      options.qs = {'fields': fields};
+    }
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function getCustomLabelsByPsid (psid) {
-  if (!psid) {
-    console.error('PSID required');
-    return;
-  }
+  return new Promise (async (resolve, reject) => {
+    if (!psid) {
+      reject('PSID required');
+    }
 
-  let options = {
-    'path': `${psid}/custom_labels`
-  }
+    let options = {
+      'path': `${psid}/custom_labels`
+    }
 
-  return this.callCustomLabelsApi(options);
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function getAllCustomLabels (fields) {
-  let options = {};
-  if (fields) {
-    fields = fields.join(',');
-    options = {
-      'qs': {'fields': fields}
+  return new Promise (async (resolve, reject) => {
+    let options = {};
+    if (fields) {
+      fields = fields.join(',');
+      options = {
+        'qs': {'fields': fields}
+      }
     }
-  }
-  return this.callCustomLabelsApi(options);
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function deleteCustomLabel (label_id) {
-  if (!label_id) {
-    console.error('label_id required');
-    return;
-  }
-  let options = {
-    'method': 'DELETE',
-    'path': label_id
-  }
+  return new Promise (async (resolve, reject) => {
+    if (!label_id) {
+      reject('label_id required');
+      return;
+    }
+    let options = {
+      'method': 'DELETE',
+      'path': label_id
+    }
 
-  return this.callCustomLabelsApi(options);
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function addPsidtoCustomLabel (psid, label_id) {
-  if (!psid || !label_id) {
-    console.error('PSID and label_id required');
-    return;
-  }
-  let options = {
-    'path': `${label_id}/label`,
-    'payload': {'user': psid}
-  }
-  return this.callCustomLabelsApi(options);
+  return new Promise (async (resolve, reject) => {
+    if (!psid || !label_id) {
+      reject('PSID and label_id required');
+      return;
+    }
+    let options = {
+      'path': `${label_id}/label`,
+      'payload': {'user': psid}
+    }
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function removePsidfromCustomLabel (psid, label_id) {
-  if (!psid || !label_id) {
-    console.error('PSID and label_id required');
-    return;
-  }
-  let options = {
-    'method': 'DELETE',
-    'path': `${label_id}/label`,
-    'payload': {'user': psid}
-  }
-  return this.callCustomLabelsApi(options);
+  return new Promise (async (resolve, reject) => {
+    if (!psid || !label_id) {
+      reject('PSID and label_id required');
+      return;
+    }
+    let options = {
+      'method': 'DELETE',
+      'path': `${label_id}/label`,
+      'payload': {'user': psid}
+    }
+    let response = this.callCustomLabelsApi(options);
+    resolve(response);
+  });
 }
 
 function callCustomLabelsApi (options) {
-  let request_options = {'api_version': 'v2.11'};
-  
-  if (options.path) {
-    request_options.path = `/${options.path}`;
-  } else {
-    request_options.path = '/me/custom_labels';
-  }
+  return new Promise (async (resolve, reject) => {
+    let request_options = {'api_version': 'v2.11'};
+    
+    if (options.path) {
+      request_options.path = `/${options.path}`;
+    } else {
+      request_options.path = '/me/custom_labels';
+    }
 
-  if (options.method) request_options.method = options.method;
+    if (options.method) request_options.method = options.method;
 
-  if (options.payload) request_options.payload = options.payload;
-  
-  return this.sendGraphRequest(request_options);
+    if (options.payload) request_options.payload = options.payload;
+    
+    let response = this.sendGraphRequest(request_options);
+    resolve(response);
+  });
 }
 
 module.exports = {
